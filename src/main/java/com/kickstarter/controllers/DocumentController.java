b63951cfc1e3143fd032c53b9341b93e59745dfc
Dima Kudosh <dimakudosh@gmail.com>
2016-04-23 22:08:58 +0300

    Create base pdf template

---
+++ src/main/java/com/kickstarter/controllers/DocumentController.java
@@ -0,0 +1,20 @@
+package com.kickstarter.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import com.kickstarter.logic.domain.User;
+
+@Controller
+public class DocumentController {
+
+    @RequestMapping(value = "/downloadPDF", method = RequestMethod.GET)
+    public ModelAndView downloadPDF() {
+        List<User> listProject = new ArrayList<User>();
+        return new ModelAndView("pdfView", "project", listProject);
+    }
+}
